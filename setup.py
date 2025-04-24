95b88c7786c966b978f6484dd07f9401900dcfcb
re7w12u <bessiere.julien@gmail.com>
2025-04-24 19:11:11 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(     
+     name="pdebugger",     
+     version="0.0.1",
+     python_requires=">=3.6",   
+     packages=["pdebugger"],
+     install_requires=['debugpy']
+)
