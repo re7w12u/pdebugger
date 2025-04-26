34a9a9da67a7f1b391984f22f861738817bd5e13
re7w12u <bessiere.julien@gmail.com>
2025-04-26 09:25:00 +0200

    add print to follow debugger setup

--- pdebugger/main.py
+++ pdebugger/main.py
@@ -1,15 +1,22 @@
 from os import getenv
 
 def initialize_debugger_if_needed(port_number):
+    print("initialing debugger 1")
     if getenv("DEBUGGER") == "True":        
         import debugpy
-        
+        print("initialing debugger 2")
+      
         if port_number is None and getenv("DEBUGGER_PORT") is not None:
             port_number = int(getenv("DEBUGGER_PORT"))
 
+        print("initialing debugger 3")
+        print(port_number)
+        
         if port_number is None:
             raise Exception("No port number provided for debugger. Set DEBUGGER_PORT environment variable or pass it as an argument.")
-        
+
+        print("initialing debugger 4")
+      
         debugpy.listen(("0.0.0.0", port_number))
         print("⏳ VS Code debugger can now be attached, press F5 in VS Code ⏳", flush=True)
         debugpy.wait_for_client()
